dde65e0c86b2de867225213ea7f85f22fc2539ac
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-04-30 16:07:42 -0400

    Clean up and document params_viewer

--- src/motile_plugin/widgets/params_viewer.py
+++ src/motile_plugin/widgets/params_viewer.py
@@ -11,6 +11,17 @@ from qtpy.QtWidgets import (
 
 class ParamView(QWidget):
     def __init__(self, param_name: str, solver_params: SolverParams):
+        """A widget for viewing a parameter (read only). Can be updated from
+        the backend by calling update_from_params with a new SolverParams
+        object.
+
+        Args:
+            param_name (str): The name of the parameter to view in this UI row.
+                Must correspond to one of the attributes of SolverParams.
+            solver_params (SolverParams): The SolverParams object to use to
+                initialize the view. Provides the title to display and the
+                initial value.
+        """
         super().__init__()
         self.param_name = param_name
         title = QLabel(solver_params.model_fields[param_name].title)
@@ -25,6 +36,14 @@ class ParamView(QWidget):
         self.update_from_params(solver_params)
 
     def update_from_params(self, params: SolverParams):
+        """Updates the current parameter value displayed in self.param_value,
+        or hides the whole row if the value is None.
+
+        Args:
+            params (SolverParams): The solver parameters we use to update
+                the UI. This object uses self.param_name to know which value
+                to retrieve.
+        """
         param_val = params.__getattribute__(self.param_name)
         if param_val is not None:
             self.show()
@@ -39,15 +58,16 @@ class ParamView(QWidget):
 
 
 class SolverParamsViewer(QWidget):
-    """ Widget for viewing SolverParams. To update for a backend change to 
-    SolverParams, emit the new_params signal, which Labels will connect to and 
-    use to update the UI.
+    """Widget for viewing SolverParams. To update for a backend change to
+    SolverParams, emit the new_params signal, which each parameter label
+    will connect to and use to update the UI.
     """
+
     new_params = Signal(SolverParams)
 
-    def __init__(self, solver_params: SolverParams):
+    def __init__(self):
         super().__init__()
-        self.solver_params = solver_params
+        self.solver_params: SolverParams = SolverParams()
         self.param_categories = {
             "hyperparams": ["max_edge_distance", "max_children"],
             "costs": [
@@ -61,31 +81,37 @@ class SolverParamsViewer(QWidget):
             ],
         }
         main_layout = QVBoxLayout()
-        main_layout.addWidget(self._ui_params_group(title="Hyperparameters", param_category="hyperparams"))
-        main_layout.addWidget(self._ui_params_group(title="Costs", param_category="costs"))
+        main_layout.addWidget(
+            self._params_group(
+                title="Hyperparameters", param_category="hyperparams"
+            )
+        )
+        main_layout.addWidget(
+            self._params_group(title="Costs", param_category="costs")
+        )
         main_layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(main_layout)
 
-    def _ui_params_group(self, title, param_category) -> QGroupBox:
+    def _params_group(self, title: str, param_category: str) -> QGroupBox:
+        """A helper function to create a group of parameters in the UI.
+        Also connects each parameter to the self.new_params signal so the
+        values can be updated when new parameters are viewed.
+
+        Args:
+            title (str): The title to put at the top of the group
+            param_category (str): A key of self.param_categories used to
+                get the list of parameters in this group.
+
+        Returns:
+            QGroupBox: A widget containing all the parameters in the group,
+                with static titles and values that can be updated via emitting
+                the self.new_params signal.
+        """
         group = QGroupBox(title)
         layout = QVBoxLayout()
         for param_name in self.param_categories[param_category]:
-            label = self._param_view(param_name)
-            layout.addWidget(label)
+            param_view = ParamView(param_name, self.solver_params)
+            self.new_params.connect(param_view.update_from_params)
+            layout.addWidget(param_view)
         group.setLayout(layout)
         return group
-
-    def _param_view(self, param_name) -> ParamView:
-        """Helper function to 
-
-        Args:
-            param_name (_type_): _description_
-
-        Returns:
-            ParamView: _description_
-        """
-        
-        param_label = ParamView(param_name, self.solver_params)
-        param_label.update_from_params(self.solver_params)
-        self.new_params.connect(param_label.update_from_params)
-        return param_label
