b43f35015804415e0c2589a8238d690c3b6dfb0f
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-05-08 14:21:56 +0200

    Black and ruff format scripts

--- scripts/prepare_example_data.py
+++ scripts/prepare_example_data.py
@@ -1,9 +1,10 @@
-from urllib.request import urlretrieve
 import zipfile
 from pathlib import Path
+from urllib.request import urlretrieve
+
 import tifffile
 import zarr
-from cellpose import models
+
 
 def download_hela_dataset(data_dir: Path):
     """Download the Fluo-N2DL-HeLa dataset from the Cell Tracking Challenge
@@ -12,9 +13,10 @@ def download_hela_dataset(data_dir: Path):
     path = "http://data.celltrackingchallenge.net/training-datasets/Fluo-N2DL-HeLa.zip"
     zip_filename = data_dir / "Fluo-N2DL-HeLa.zip"
     urlretrieve(path, filename=zip_filename)
-    with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
+    with zipfile.ZipFile(zip_filename, "r") as zip_ref:
         zip_ref.extractall(data_dir)
     zip_filename.unlink()
+
 
 def convert_to_zarr(tif_path: Path, zarr_path: Path, zarr_group: str):
     """Convert tiff file image data to zarr"""
@@ -27,13 +29,17 @@ def convert_to_zarr(tif_path: Path, zarr_path: Path, zarr_group: str):
     # prepare zarr
     zarr_path.mkdir(parents=True, exist_ok=True)
     store = zarr.NestedDirectoryStore(zarr_path)
-    zarr_array = zarr.open(store=store, mode='w', path=zarr_group, shape=data_shape, dtype=data_dtype)
+    zarr_array = zarr.open(
+        store=store,
+        mode="w",
+        path=zarr_group,
+        shape=data_shape,
+        dtype=data_dtype,
+    )
     # load and save data in zarr
     for t, file in enumerate(files):
         image = tifffile.imread(file)
         zarr_array[t] = image
-        
-
 
 
 if __name__ == "__main__":
--- scripts/run_hela.py
+++ scripts/run_hela.py
@@ -1,13 +1,13 @@
+import logging
+
 import napari
 import zarr
-
 from motile_plugin import MotileWidget
 from napari.utils.theme import _themes
-import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(filename)s:%(lineno)d] %(levelname)-8s %(message)s"
+    format="%(asctime)s [%(filename)s:%(lineno)d] %(levelname)-8s %(message)s",
 )
 # logging.getLogger('napari').setLevel(logging.DEBUG)
 
@@ -16,14 +16,14 @@ _themes["dark"].font_size = "18pt"
 
 # Load Zarr datasets
 zarr_directory = "../data/Fluo-N2DL-HeLa.zarr"
-zarr_group = zarr.open_group(zarr_directory, mode='r')
+zarr_group = zarr.open_group(zarr_directory, mode="r")
 
 # Initialize Napari viewer
 viewer = napari.Viewer()
 
 # Add image and label layers to the viewer
-viewer.add_image(zarr_group['01'][:], name='01 Raw')
-viewer.add_labels(zarr_group['01_ST'][:], name='01 ST')
+viewer.add_image(zarr_group["01"][:], name="01 Raw")
+viewer.add_labels(zarr_group["01_ST"][:], name="01 ST")
 
 # Add your custom widget
 widget = MotileWidget(viewer)
