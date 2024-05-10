3f24204009a520c5a362ff83e164120f02df67af
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-05-10 16:43:01 +0200

    Make tests actually pass

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,11 +1,10 @@
 from motile_plugin.backend.solve import solve
-from motile_plugin.backend.solver_params import SolverParamsWidget
+from motile_plugin.backend.solver_params import SolverParams
 
 
 # capsys is a pytest fixture that captures stdout and stderr output streams
 def test_solve_2d(segmentation_2d, graph_2d):
-    params = SolverParamsWidget()
-    params.merge_cost = None
+    params = SolverParams()
     params.appear_cost = None
     params.disappear_cost = None
     soln_graph = solve(params, segmentation_2d)
@@ -13,8 +12,7 @@ def test_solve_2d(segmentation_2d, graph_2d):
 
 
 def test_solve_3d(segmentation_3d, graph_3d):
-    params = SolverParamsWidget()
-    params.merge_cost = None
+    params = SolverParams()
     params.appear_cost = None
     params.disappear_cost = None
     soln_graph = solve(params, segmentation_3d)
