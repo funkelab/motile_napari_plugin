bdf096a3a8c8141c6607194b4077b1b59d673267
AnniekStok <anniek.stokkermans@gmail.com>
2024-07-30 10:20:17 +0200

    update treewidget display according to selected nodes and center view

--- src/motile_plugin/widgets/tree_widget.py
+++ src/motile_plugin/widgets/tree_widget.py
@@ -4,7 +4,7 @@ import napari
 import numpy as np
 import pyqtgraph as pg
 from psygnal import Signal
-from PyQt5.QtGui import QMouseEvent
+from PyQt5.QtGui import QColor, QMouseEvent
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QHBoxLayout, QWidget
 
@@ -32,6 +32,7 @@ class TreeWidget(QWidget):
         self.colormap = view_controller.colormap
         self.node_selected.connect(view_controller.select_node)
         view_controller.tracking_layers_updated.connect(self.update_track_data)
+        view_controller.selection_updated.connect(self._show_selected)
         # self.selected_nodes.list_updated.connect(self._show_selected)
 
         # Construct the tree view pyqtgraph widget
@@ -71,14 +72,27 @@ class TreeWidget(QWidget):
         append = Qt.ShiftModifier == modifiers
         self.node_selected.emit(node_df.iloc[0].to_dict(), append)
 
-    def _show_selected(self):
+    def _show_selected(self, selection):
         """Update the graph, increasing the size of selected node(s)"""
 
         size = (
             self.size.copy()
         )  # just copy the size here to keep the original self.size intact
-        for node in self.selected_nodes:
+        
+        outlines = self.outline_pen.copy()
+
+        for i, node in enumerate(selection):
             size[node["index"]] = size[node["index"]] + 5
+            outlines[node["index"]] = pg.mkPen(color="c", width=2)           
+            if i == 0:
+                x_coord = self.track_data.df.loc[
+                (self.track_data.df["node_id"] == node['node_id']) & 
+                (self.track_data.df["t"] == node['t']), 
+                "x_axis_pos"
+                ].iloc[0]
+                self._center_view(x_coord, node["t"])
+
+        self.tree_widget.plotItem.items[0].scatter.opts["pen"] = outlines
 
         self.g.setData(
             pos=self.pos,
@@ -142,6 +156,10 @@ class TreeWidget(QWidget):
         self.pen = np.array(adj_colors)
         self.size = np.array(sizes)
 
+        self.outline_pen = np.array(
+            [pg.mkPen(QColor(150, 150, 150)) for i in range(len(self.pos))]
+        )
+
         if len(self.pos) > 0:
             self.g.setData(
                 pos=self.pos,
@@ -151,6 +169,9 @@ class TreeWidget(QWidget):
                 size=self.size,
                 pen=self.pen,
             )
+            self.tree_widget.plotItem.items[0].scatter.opts[
+                "pen"
+            ] = self.outline_pen
         else:
             self.g.scatter.clear()
 
@@ -217,3 +238,38 @@ class TreeWidget(QWidget):
             size=self.size,
             pen=self.pen,
         )
+
+    def _center_view(self, center_x: int, center_y: int):
+        """Center the Viewbox on given coordinates"""
+
+        view_box = self.tree_widget.plotItem.getViewBox()
+        current_range = (
+            view_box.viewRange()
+        )  # Get the current range of the viewbox
+
+        x_range = current_range[0]
+        y_range = current_range[1]
+
+        # Check if the new center is within the current range
+        if (
+            x_range[0] <= center_x <= x_range[1]
+            and y_range[0] <= center_y <= y_range[1]
+        ):
+            return  # The point is already within the current range, no need to move the view
+
+        # Calculate the width and height of the current view
+        current_width = x_range[1] - x_range[0]
+        current_height = y_range[1] - y_range[0]
+
+        # Calculate new ranges maintaining the current width and height
+        new_x_range = (
+            center_x - current_width / 2,
+            center_x + current_width / 2,
+        )
+        new_y_range = (
+            center_y - current_height / 2,
+            center_y + current_height / 2,
+        )
+
+        # Set the new range to the viewbox
+        view_box.setRange(xRange=new_x_range, yRange=new_y_range, padding=0)
