86a2718da6177a06bebf75c52f8dbae8e1989870
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-08-20 11:05:44 -0400

    Add docstrings to new core classes

--- src/motile_plugin/core/node_type.py
+++ src/motile_plugin/core/node_type.py
@@ -2,8 +2,10 @@ from enum import Enum
 
 
 class NodeType(Enum):
+    """Types of nodes in the track graph. Currently used for standardizing
+    visualization. All nodes are exactly one type.
+    """
+
     SPLIT = "SPLIT"
     END = "END"
-    MERGE = "MERGE"
-    START = "START"
     CONTINUE = "CONTINUE"
