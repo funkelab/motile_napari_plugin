6c796980bc49ec2ef33493c23724da4d20a798a6
AnniekStok <anniek.stokkermans@gmail.com>
2024-09-16 11:08:44 -0400

    save the scaling information in the motile_run and load it back in when opening a previous run

--- src/motile_plugin/backend/motile_run.py
+++ src/motile_plugin/backend/motile_run.py
@@ -17,6 +17,7 @@ IN_POINTS_FILEANME = "input_points.npy"
 OUT_SEG_FILEANME = "output_segmentation.npy"
 TRACKS_FILENAME = "tracks.json"
 GAPS_FILENAME = "gaps.txt"
+SCALE_FILENAME = "scale.txt"
 
 
 class MotileRun(BaseModel):
@@ -99,7 +100,12 @@ class MotileRun(BaseModel):
                 )
             if self.tracks.graph is not None:
                 self._save_tracks_graph(run_dir, self.tracks.graph)
-        self._save_gaps(run_dir)
+        self._save_list(
+            list_to_save=self.gaps, run_dir=run_dir, filename=GAPS_FILENAME
+        )
+        self._save_list(
+            list_to_save=self.scale, run_dir=run_dir, filename=SCALE_FILENAME
+        )
         return run_dir
 
     @classmethod
@@ -138,7 +144,8 @@ class MotileRun(BaseModel):
             run_dir, required=output_required
         )
         tracks = Tracks(graph=tracks_graph, segmentation=output_segmentation)
-        gaps = cls._load_gaps(run_dir)
+        gaps = cls._load_list(run_dir=run_dir, filename=GAPS_FILENAME)
+        scale = cls._load_list(run_dir=run_dir, filename=SCALE_FILENAME)
         return cls(
             run_name=run_name,
             solver_params=params,
@@ -147,6 +154,7 @@ class MotileRun(BaseModel):
             tracks=tracks,
             time=time,
             gaps=gaps,
+            scale=scale,
         )
 
     def _save_params(self, run_dir: Path):
@@ -265,23 +273,25 @@ class MotileRun(BaseModel):
         else:
             return None
 
-    def _save_gaps(self, run_dir: Path):
-        gaps_file = run_dir / GAPS_FILENAME
-        with open(gaps_file, "w") as f:
-            f.write(",".join(map(str, self.gaps)))
+    def _save_list(self, list_to_save: list, run_dir: Path, filename: str):
+        list_file = run_dir / filename
+        with open(list_file, "w") as f:
+            f.write(",".join(map(str, list_to_save)))
 
     @staticmethod
-    def _load_gaps(run_dir, required: bool = True) -> list[float]:
-        gaps_file = run_dir / GAPS_FILENAME
-        if gaps_file.is_file():
-            with open(gaps_file) as f:
+    def _load_list(
+        run_dir: Path, filename: str, required: bool = True
+    ) -> list[float]:
+        list_file = run_dir / filename
+        if list_file.is_file():
+            with open(list_file) as f:
                 file_content = f.read()
             if file_content == "":
                 return []
             gaps = list(map(float, file_content.split(",")))
             return gaps
         elif required:
-            raise FileNotFoundError(f"No gaps found at {gaps_file}")
+            raise FileNotFoundError(f"No content found at {list_file}")
         else:
             return []
 
@@ -301,4 +311,5 @@ class MotileRun(BaseModel):
         (run_dir / OUT_SEG_FILEANME).unlink()
         (run_dir / TRACKS_FILENAME).unlink()
         (run_dir / GAPS_FILENAME).unlink()
+        (run_dir / SCALE_FILENAME).unlink()
         run_dir.rmdir()
