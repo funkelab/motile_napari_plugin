0566ecfc304d3f1318710b97ab1c16b91e3ba41b
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-12-03 13:34:46 -0500

    Refactor scale spinbox creation into a function

--- src/motile_plugin/import_export/import_external_tracks_dialog.py
+++ src/motile_plugin/import_export/import_external_tracks_dialog.py
@@ -32,21 +32,9 @@ class ScaleWidget(QWidget):
         layout = QVBoxLayout()
         layout.addWidget(QLabel("Specify scaling"))
         scale_form_layout = QFormLayout()
-        self.z_spin_box = QDoubleSpinBox()
-        self.z_spin_box.setValue(1.0)
-        self.z_spin_box.setSingleStep(0.1)
-        self.z_spin_box.setMinimum(0)
-        self.z_spin_box.setDecimals(3)
-        self.y_spin_box = QDoubleSpinBox()
-        self.y_spin_box.setValue(1.0)
-        self.y_spin_box.setSingleStep(0.1)
-        self.y_spin_box.setMinimum(0)
-        self.y_spin_box.setDecimals(3)
-        self.x_spin_box = QDoubleSpinBox()
-        self.x_spin_box.setMinimum(0)
-        self.x_spin_box.setValue(1.0)
-        self.x_spin_box.setSingleStep(0.1)
-        self.x_spin_box.setDecimals(3)
+        self.z_spin_box = self._scale_spin_box()
+        self.y_spin_box = self._scale_spin_box()
+        self.x_spin_box = self._scale_spin_box()
 
         scale_form_layout.addRow(QLabel("z"), self.z_spin_box)
         scale_form_layout.addRow(QLabel("y"), self.y_spin_box)
@@ -57,10 +45,20 @@ class ScaleWidget(QWidget):
 
         self.setLayout(layout)
 
+    def _scale_spin_box(self) -> QDoubleSpinBox:
+        spin_box = QDoubleSpinBox()
+        spin_box.setValue(1.0)
+        spin_box.setSingleStep(0.1)
+        spin_box.setMinimum(0)
+        spin_box.setDecimals(3)
+        return spin_box
+
     def get_scale(self) -> list[float]:
-        """Return the scaling values in the spinboxes as a list of floats"""
+        """Return the scaling values in the spinboxes as a list of floats.
+        Since we currently require a dummy 1 value for the time dimension, add it here."""
 
         scale = [
+            1,
             self.z_spin_box.value(),
             self.y_spin_box.value(),
             self.x_spin_box.value(),
