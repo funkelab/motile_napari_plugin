d51bd7b12184d448d44b7a5254c7539e5633cde2
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-12-19 13:52:40 -0500

    Update motile and utils tests

--- tests/motile_plugin/utils/test_tree_widget_utils.py
+++ tests/motile_plugin/utils/test_tree_widget_utils.py
@@ -10,8 +10,8 @@ from motile_tracker.data_views.views.tree_view.tree_widget_utils import (
 def test_track_df(graph_2d):
     tracks = SolutionTracks(graph=graph_2d, ndim=3)
 
-    assert tracks.get_area("0_1") == 1245
-    assert tracks.get_area("1_2") is None
+    assert tracks.get_area(1) == 1245
+    assert tracks.get_area(2) is None
 
     tracks.graph, _, _ = assign_tracklet_ids(tracks.graph)
 
@@ -23,6 +23,6 @@ def test_track_df(graph_2d):
 
     track_df = extract_sorted_tracks(tracks, colormap)
     assert isinstance(track_df, pd.DataFrame)
-    assert track_df.loc[track_df["node_id"] == "0_1", "area"].values[0] == 1245
-    assert track_df.loc[track_df["node_id"] == "1_2", "area"].values[0] == 0
+    assert track_df.loc[track_df["node_id"] == 1, "area"].values[0] == 1245
+    assert track_df.loc[track_df["node_id"] == 2, "area"].values[0] == 0
     assert track_df["area"].notna().all()
